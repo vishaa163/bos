323ee659bbb297dda18a11cf8fefca5364bcdfa0
vishaa163 <130795602+vishaa163@users.noreply.github.com>
2024-11-13 17:05:06 +0300

    Delete artestc-main directory

--- artestc-main/2_work/libmysyslog-json/include/mysyslog-json.h
+++
@@ -1,6 +0,0 @@
-#ifndef MYSYSLOG_JSON_H
-#define MYSYSLOG_JSON_H
-
-int log_json(const char* msg, int level, const char* path);
-
-#endif // MYSYSLOG_JSON_H
--- artestc-main/2_work/libmysyslog-text/include/mysyslog-text.h
+++
@@ -1,6 +0,0 @@
-#ifndef MYSYSLOG_TEXT_H
-#define MYSYSLOG_TEXT_H
-
-int log_text(const char* msg, int level, const char* path);
-
-#endif // MYSYSLOG_TEXT_H
